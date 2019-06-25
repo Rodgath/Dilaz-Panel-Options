04be4fbe702c9f977becf00073001ef9f39ed957
Peter Rodgath <rodgath@gmail.com>
2019-06-25 06:05:43 +0300

    Update load.php

--- includes/load.php
+++ includes/load.php
@@ -147,9 +147,7 @@ if (!is_plugin_active(DILAZ_PANEL_PLUGIN_FILE)) {
 			$activation_url = wp_nonce_url('plugins.php?action=activate&amp;plugin='. urlencode(DILAZ_PANEL_PLUGIN_FILE), 'activate-plugin_'. DILAZ_PANEL_PLUGIN_FILE);
 			echo '<div id="message" class="dilaz-panel-notice notice notice-warning"><p><strong>'. sprintf(__('Please %1$sactivate%2$s <em>Dilaz Panel</em> plugin. It is required by "<em>%3$s</em>" %4$s.', 'dilaz-panel'), '<a href="'. $activation_url .'">', '</a>', $item_name, $item_type) .'</strong></p></div>';
 		} else {
-			add_action('admin_notices', function() {
-				echo '<div id="message" class="dilaz-panel-notice notice notice-warning"><p><strong>'. sprintf(__('Please %1$sinstall%2$s <em>Dilaz Panel</em> plugin. It is required by "<em>%3$s</em>" %4$s.', 'dilaz-panel'), '<a href="'. admin_url('plugin-install.php?tab=upload') .'">', '</a>', $item_name, $item_type) .'</strong></p></div>';
-			});
+			echo '<div id="message" class="dilaz-panel-notice notice notice-warning"><p><strong>'. sprintf(__('Please %1$sinstall%2$s <em>Dilaz Panel</em> plugin. It is required by "<em>%3$s</em>" %4$s.', 'dilaz-panel'), '<a href="'. admin_url('plugin-install.php?tab=upload') .'">', '</a>', $item_name, $item_type) .'</strong></p></div>';
 		}
 	});
 	
