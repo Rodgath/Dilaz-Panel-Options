c82460fe181cdce7195d822f2c08c76af5a5436a
Peter Rodgath <rodgath@gmail.com>
2019-06-26 18:33:14 +0300

    Update use-type.php

--- includes/use-type.php
+++ includes/use-type.php
@@ -61,7 +61,7 @@ if (isset($parameters['use_type']) && $parameters['use_type'] == 'theme') {
 		# check if panel is used in 'theme' or 'plugin'
 		$current_use_type = 'theme' == dilaz_panel_get_use_type() ? 'theme' : 'plugin';
 	
-		echo '<div id="message" class="error"><p><strong>'. sprintf( __( 'Wrong "use type" for admin options. Please set "<em>use_type</em>" parameter to "<em>%1$s</em>" in "<em>%2$s</em>".', 'dilaz-panel' ), $current_use_type, 'wp-content'. wp_normalize_path(explode('wp-content', dirname(__DIR__))[1]) .'/config.php' ) .'</strong></p></div>';
+		echo '<div id="message" class="dilaz-panel-notice notice notice-warning error"><p><strong>'. sprintf( __( 'Wrong "use type" for admin options. Please set "<em>use_type</em>" parameter to "<em>%1$s</em>" in "<em>%2$s</em>".', 'dilaz-panel' ), $current_use_type, 'wp-content'. wp_normalize_path(explode('wp-content', dirname(__DIR__))[1]) .'/config.php' ) .'</strong></p></div>';
 	});
 	
 	# set use type error
