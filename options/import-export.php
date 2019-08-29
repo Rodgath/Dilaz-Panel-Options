7eb1a7493fe1643850bfe96577aae4ca15d65198
Rodgath <rodgath@gmail.com>
2019-08-29 20:50:11 +0300

    Update import-export.php

--- options/import-export.php
+++ options/import-export.php
@@ -4,13 +4,13 @@
 || Default Panel Import/Export Option Fields
 || --------------------------------------------------------------------------------------------
 ||
-|| @package		Dilaz Panel
-|| @subpackage	Import/Export Fields
-|| @since		Dilaz Panel 1.1
-|| @author		Rodgath, https://github.com/Rodgath
-|| @copyright	Copyright (C) 2017, Rodgath LTD
-|| @link		https://github.com/Rodgath/Dilaz-Panel-Plugin
-|| @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+|| @package    Dilaz Panel
+|| @subpackage Import/Export Fields
+|| @since      Dilaz Panel 1.1
+|| @author     Rodgath, https://github.com/Rodgath
+|| @copyright  Copyright (C) 2017, Rodgath LTD
+|| @link       https://github.com/Rodgath/Dilaz-Panel-Plugin
+|| @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 || 
 */
 
